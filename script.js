fef3bb35e7c9969ef8418d08cbe44fdddd491d91
Anjal Thapa <71475450+Aerackkkkk@users.noreply.github.com>
2023-02-16 11:17:33 +0545

    Add files via upload
    Simple arithmetic addition

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+let x=5;
+let z=10;
+let add=x+z;
+let sub=x-z;
+let mul=x*z;
+document.getElementById("sum").innerHTML = add;
+document.getElementById("sub").innerHTML = sub;
+document.getElementById("mul").innerHTML = mul;
